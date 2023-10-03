ad4282f99ac59d7e541e4692ea86d871a9fe03e5
Romit <romitmpatel.2001@gmail.com>
2023-10-03 01:56:48 -0400

    Refactor continent test class

--- src/test/java/com/riskgame/model/ContinentTest.java
+++ src/test/java/com/riskgame/model/ContinentTest.java
@@ -17,12 +17,12 @@ public class ContinentTest {
 
     @Before
     public void before() {
-        d_gameMap = new GameMap("ameroki.map");
-        d_mapHelper = new MapHelper();
-        d_continent = new Continent();
-        d_continentId = "eurozio";
-        d_controlValue = 5;
-        d_continent.isContinentAdded(d_gameMap, "azio", d_controlValue);
+        this.d_gameMap = new GameMap("ameroki.map");
+        this.d_mapHelper = new MapHelper();
+        this.d_continent = new Continent();
+        this.d_continentId = "eurozio";
+        this.d_controlValue = 5;
+        this.d_continent.isContinentAdded(this.d_gameMap, "azio", this.d_controlValue);
     }
 
     /**
@@ -30,11 +30,11 @@ public class ContinentTest {
      */
     @Test
     public void testAddContinent() {
-        d_gameMap = d_mapHelper.editMap("ameroki.map");
-        System.out.println(d_gameMap.getMapName());
-        System.out.println(d_gameMap.getContinents().size());
+        this.d_gameMap = this.d_mapHelper.editMap("ameroki.map");
+        System.out.println(this.d_gameMap.getMapName());
+        System.out.println(this.d_gameMap.getContinents().size());
 
-        boolean l_check = d_continent.isContinentAdded(d_gameMap, d_continentId, d_controlValue);
+        boolean l_check = this.d_continent.isContinentAdded(this.d_gameMap, this.d_continentId, this.d_controlValue);
         assertTrue(l_check);
     }
 
@@ -43,10 +43,10 @@ public class ContinentTest {
      */
     @Test
     public void testRemoveContinent() {
-        d_gameMap = d_mapHelper.editMap("ameroki.map");
+        this.d_gameMap = this.d_mapHelper.editMap("ameroki.map");
         Continent l_continent = new Continent("azio", 5);
 
-        boolean l_check = l_continent.removeContinent(d_gameMap);
+        boolean l_check = l_continent.removeContinent(this.d_gameMap);
         assertTrue(l_check);
     }
 
@@ -57,13 +57,13 @@ public class ContinentTest {
     public void testEditContinentAddingExistingContinent() {
         String l_continentId = "azio";
         int l_controlValue = 7;
-        d_continent.isContinentAdded(d_gameMap, l_continentId, l_controlValue);
+        this.d_continent.isContinentAdded(this.d_gameMap, l_continentId, l_controlValue);
 
         String[] args = { "editcontinent", "-add", l_continentId, Integer.toString(l_controlValue) };
-        d_gameMap.getContinents().clear(); // Clear existing continents
-        d_continent.editContinent(d_gameMap, Phase.EDITMAP, args);
+        this.d_gameMap.getContinents().clear(); // Clear existing continents
+        this.d_continent.editContinent(this.d_gameMap, Phase.EDITMAP, args);
 
-        assertEquals(l_controlValue, d_gameMap.getContinents().get(l_continentId.toLowerCase()).getControlValue());
+        assertEquals(l_controlValue, this.d_gameMap.getContinents().get(l_continentId.toLowerCase()).getControlValue());
     }
 
     /**
@@ -73,10 +73,10 @@ public class ContinentTest {
     @Test
     public void testEditContinentRemovingNonexistentContinent() {
         String[] args = { "editcontinent", "-remove", "NonexistentContinent" };
-        d_gameMap.getContinents().clear(); // Clear existing continents
-        d_continent.editContinent(d_gameMap, Phase.EDITMAP, args);
+        this.d_gameMap.getContinents().clear(); // Clear existing continents
+        this.d_continent.editContinent(this.d_gameMap, Phase.EDITMAP, args);
 
-        assertTrue(d_gameMap.getContinents().isEmpty());
+        assertTrue(this.d_gameMap.getContinents().isEmpty());
     }
 
     /**
@@ -85,9 +85,9 @@ public class ContinentTest {
     @Test
     public void testEditContinentWithInvalidArguments() {
         String[] args = { "editcontinent", "-add", "Invalid$Continent", "4" };
-        d_gameMap.getContinents().clear();
-        d_continent.editContinent(d_gameMap, Phase.EDITMAP, args);
+        this.d_gameMap.getContinents().clear();
+        this.d_continent.editContinent(this.d_gameMap, Phase.EDITMAP, args);
 
-        assertTrue(d_gameMap.getContinents().isEmpty());
+        assertTrue(this.d_gameMap.getContinents().isEmpty());
     }
 }

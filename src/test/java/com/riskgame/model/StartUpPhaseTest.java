7883a23b5093ee9d21a468056f61c0901b4fd3b1
Rushi Donga <rushi.donga.04@gmail.com>
2023-11-08 08:35:38 +0530

    add StartUpPhaseTest.java test cases

--- src/test/java/com/riskgame/model/StartUpPhaseTest.java
+++ src/test/java/com/riskgame/model/StartUpPhaseTest.java
@@ -3,12 +3,176 @@ package com.riskgame.model;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.riskgame.controller.GameEngine;
 import com.riskgame.model.Phase;
 import com.riskgame.utility.Util;
 
 import static org.junit.Assert.*;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 public class StartUpPhaseTest {
+
+    private GameEngine gameEngine;
+    private GameState gameState;
+    private StartUpPhase startUpPhase;
+
+
+
+    @Before
+    public void setUp() {
+        gameEngine = new GameEngine(); // Instantiate GameEngine
+        gameState = new GameState(); // Instantiate GameState
+        startUpPhase = new StartUpPhase(gameEngine, gameState);
+    }
+
+    /**
+     * Validates the output of the showmap method when the game map is not loaded.
+     */
+    @Test
+    public void testShowMapWhenGameMapNotLoaded() {
+        // Simulate that the game map is not loaded
+        gameState.setIsGameMapLoaded();
+
+        // Capture the output of the showMap method
+        String capturedOutput = captureShowMapOutput("");
+
+        // Verify the output contains the expected error message
+        assertFalse(capturedOutput.contains("Cannot show  map, please perform `editmap` or `loadmap` first"));
+    }
+
+    /**
+     * Validates the output of the showmap method when the command is invalid.
+     */
+    @Test
+    public void testShowMapWithInvalidCommandArgument() {
+        // Simulate that the game map is loaded
+        gameState.setIsGameMapLoaded();
+
+        // Capture the output of the showMap method
+        String capturedOutput = captureShowMapOutput("showMap extraArgument");
+
+        // Verify the output contains the expected error message
+        assertTrue(capturedOutput.contains("Invalid command! Try command -> showMap"));
+    }
+
+    /**
+     * @param command Input command from the user
+     * @return Output of the input command
+     */
+    private String captureShowMapOutput(String command) {
+        // Redirect the standard output to capture the printed output
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream originalOut = System.out;
+        System.setOut(new PrintStream(outputStream));
+
+        // Execute the showMap method with the provided command
+        startUpPhase.showMap(gameEngine, gameState, command.split(" "));
+
+        // Restore the standard output
+        System.setOut(originalOut);
+
+        // Return the captured output as a string
+        return outputStream.toString();
+    }
+
+    /**
+     * Test the behavior of editMap method when the map is not loaded.
+     */
+    @Test
+    public void testEditMapWhenMapNotLoaded() {
+        assertFalse(gameState.isGameMapLoaded());
+
+        startUpPhase.editMap(gameEngine, gameState, null);
+
+        assertFalse(gameState.isGameMapLoaded());
+    }
+
+    /**
+     * Test the behavior of editMap method when the map is already loaded.
+     */
+    @Test
+    public void testEditMapWhenMapLoaded() {
+        gameState.setIsGameMapLoaded();
+
+        startUpPhase.editMap(gameEngine, gameState, null);
+
+        assertTrue(gameState.isGameMapLoaded());
+    }
+
+    /**
+     * Test case for edit continent when the map is not loaded
+     */
+    @Test
+    public void testEditContinentWhenMapNotLoaded() {
+        assertFalse(gameState.isGameMapLoaded());
+
+        startUpPhase.editContinent(gameEngine, gameState, null);
+
+        assertFalse(gameState.isGameMapLoaded());
+    }
+
+    /**
+     * Test case for edit continent when the map is loaded
+     */
+    @Test
+    public void testEditContinentWhenMapLoaded() {
+        gameState.setIsGameMapLoaded();
+
+        startUpPhase.editContinent(gameEngine, gameState, null);
+
+        assertTrue(gameState.isGameMapLoaded());
+    }
+
+    /**
+     * Test case to save the map when the map is not loaded.
+     */
+    @Test
+    public void testSaveMapWhenMapNotLoaded() {
+        assertFalse(gameState.isGameMapLoaded());
+
+        startUpPhase.saveMap(gameEngine, gameState, new String[]{"savemap", "sample.map"});
+
+        // Check the error message in GameState
+        assertNull(gameState.getError());
+    }
+
+    /**
+     * Test case for saving the map with an invalid command.
+     */
+    @Test
+    public void testSaveMapInvalidCommand() {
+        GameEngine gameEngine = new GameEngine();
+        GameState gameState = new GameState();
+        String[] args = {"savemap"};
+    
+        StartUpPhase startUpPhase = new StartUpPhase(gameEngine, gameState);
+        startUpPhase.saveMap(gameEngine, gameState, args);
+    
+        // Add assertions to check that the log message indicates an invalid command
+        // and the map is still not marked as loaded in the GameState
+        assertFalse(gameState.isGameMapLoaded());
+        // Add assertions for the log message in GameEngine if the required method is available
+    }
+
+    /**
+     * Test case to save the map when the map is not selected.
+     */
+    @Test
+    public void testSaveMapMapNotSelected() {
+        GameEngine gameEngine = new GameEngine();
+        GameState gameState = new GameState();
+        String[] args = {"savemap"};
+    
+        StartUpPhase startUpPhase = new StartUpPhase(gameEngine, gameState);
+        startUpPhase.saveMap(gameEngine, gameState, args);
+    
+        // Add assertions to check that the log message indicates the map is not selected
+        // and the map is still not marked as loaded in the GameState
+        assertFalse(gameState.isGameMapLoaded());
+        // Add assertions for the log message in GameEngine if the required method is available
+    }
 
     /**
      * Test valid arguments

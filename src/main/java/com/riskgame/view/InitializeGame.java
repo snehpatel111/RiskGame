05c9b9f3c474088cae392e99e284ed179d967843
Sneh Patel <patelsneh9100@gmail.com>
2023-11-28 21:17:58 -0500

    refactor InitializeGame class

--- src/main/java/com/riskgame/view/InitializeGame.java
+++ src/main/java/com/riskgame/view/InitializeGame.java
@@ -10,40 +10,45 @@ import com.riskgame.utility.Constant;
 public class InitializeGame {
     public static void main(String[] args) {
         try (Scanner sc = new Scanner(System.in)) {
-            GameEngine l_GameEngine = new GameEngine();
+            GameEngine l_gameEngine = new GameEngine();
             String l_cmd;
             String message = "";
 
             System.out.println("\n=================== Welcome To RiskGame! ======================\n");
             while (true) {
-                System.out.println("Enter S to play single-game Mode OR T to play tournament-game Mode OR Q to quit/exit the game");
+                System.out.println(
+                        "Enter S to play single-game Mode OR T to play tournament-game Mode OR Q to quit/exit the game");
                 l_cmd = sc.nextLine();
-                switch (l_cmd) {
-                    case "S":
-                        l_GameEngine.getCurrentGamePhase().getGameState().updateLog("Initializing the Game ......" + System.lineSeparator(),
+                switch (l_cmd.toLowerCase()) {
+                    case "s":
+                        l_gameEngine.getCurrentGamePhase().getGameState().updateLog(
+                                "Initializing the Game ......" + System.lineSeparator(),
                                 "start");
-                        l_GameEngine.setGameEngineLog("Game Startup Phase", "phase");
-                        l_GameEngine.getCurrentGamePhase().initPhase();
-                        
+                        l_gameEngine.setGameEngineLog("Game Startup Phase", "phase");
+                        l_gameEngine.getCurrentGamePhase().initPhase();
+
                         break;
-                    case "T":
-                        TournamentEngine l_tournamentEngine = new TournamentEngine(l_GameEngine, l_GameEngine.getGameState());
+                    case "t":
+                        TournamentEngine l_tournamentEngine = new TournamentEngine(l_gameEngine,
+                                l_gameEngine.getGameState());
                         do {
-                            System.out.println("Command has to be in form of 'tournament -M listofmapfiles{1-5} -P listofplayerstrategies{2-4}"+""
-                                                + "-G numberofgames{1-5} -D maxnumberofturns{10-50}");
+                            System.out.println(
+                                    "Command has to be in form of 'tournament -M listofmapfiles{1-5} -P listofplayerstrategies{2-4}"
+                                            + ""
+                                            + "-G numberofgames{1-5} -D maxnumberofturns{10-50}");
                             l_cmd = sc.nextLine();
                             message = l_tournamentEngine.parse(null, l_cmd);
                         } while (!message.equals("success"));
 
                         break;
 
-                    case "Q":
+                    case "q":
                         System.exit(0);
                         break;
-                
+
                     default:
-                        System.out.println(Constant.ERROR_COLOR + "Invalid Command!!\nEnter S to play" + "" +
-                                            "single-game Mode OR T to play tournament-game Mode" + Constant.RESET_COLOR);
+                        System.out.println(Constant.ERROR_COLOR + "Invalid Command!\nEnter S to play" + "" +
+                                "single-game Mode OR T to play tournament-game Mode" + Constant.RESET_COLOR);
                         break;
                 }
             }
